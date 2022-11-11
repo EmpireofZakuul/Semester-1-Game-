4660b5fa3bddeb6416a53f20428f6338c7079f9b
Valkorian <marcopolo9420@gmail.com>
2022-11-11 15:12:14 +0000

    player death

--- Semester 1 game/Assets/Scripts/Enemy.cs
+++ Semester 1 game/Assets/Scripts/Enemy.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    public float enemyMoveSpeed = 3f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
+
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        transform.Translate(Vector2.right * enemyMoveSpeed * Time.deltaTime);
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+
+        if (other.gameObject.tag == "NPCTurnRight")
+        {
+            transform.localRotation = Quaternion.Euler(0, 0, 0);
+        }
+        if (other.gameObject.tag == "NPCTurnLeft")
+        {
+            transform.localRotation = Quaternion.Euler(0, 180, 0);
+        }
     }
 }
--- Semester 1 game/Assets/Scripts/PlayerHealth.cs
+++ Semester 1 game/Assets/Scripts/PlayerHealth.cs
@@ -1,13 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerHealth : MonoBehaviour
 {
+    public bool dead;
     // Start is called before the first frame update
     void Start()
     {
-        
+        dead = false;
     }
 
     // Update is called once per frame
@@ -17,10 +19,20 @@ public class PlayerHealth : MonoBehaviour
     }
     public void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.GetComponent<Enemy>())
+        if (!dead)
         {
-            Destroy(transform.parent.gameObject);
+            if (collision.GetComponent<Enemy>())
+            {
+                Destroy(transform.parent.gameObject);
+                Die();
+            }
         }
 
+    }
+
+    public void Die()
+    {
+        dead = true;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
