8258f04f265f218b85f264fb9a7230cc7cbe468b
EmpireofZakuul <60693663+EmpireofZakuul@users.noreply.github.com>
2022-11-16 23:08:23 +0000

    added the counter to the game

---
+++ Semester 1 game/Assets/Scripts/Collectables.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class Collectables : MonoBehaviour
+{
+    public TextMeshProUGUI counterText;
+    public int counter = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        counterText.text = counter.ToString();
+        counter = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+         counterText.text = counter.ToString();
+
+    }
+   
+}
