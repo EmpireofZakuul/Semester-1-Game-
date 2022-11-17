f000ab7a3d89395d610521012ef556f03d9b5d6e
Valkorian <marcopolo9420@gmail.com>
2022-11-17 18:21:11 +0000

    s

--- Semester 1 game/Assets/Scripts/PlayerPickUp.cs
+++ Semester 1 game/Assets/Scripts/PlayerPickUp.cs
@@ -21,6 +21,7 @@ public class PlayerPickUp : MonoBehaviour
        if(collision.gameObject.tag == "Collectables")
        {
            collect.counter++;
+            Destroy(collision.gameObject);
        }
        
    }
