36ebf60aacccef9acf2c1f39978e46c01268cd39
EmpireofZakuul <60693663+EmpireofZakuul@users.noreply.github.com>
2022-11-21 19:45:52 +0000

    fixed camera

--- Semester 1 game/Assets/Scripts/CameraFollowPlayer.cs
+++ Semester 1 game/Assets/Scripts/CameraFollowPlayer.cs
@@ -5,11 +5,44 @@ using UnityEngine;
 public class CameraFollowPlayer : MonoBehaviour
 {
     public Transform player;
-    // Update is called once per frame
+    //public float followSpeed = 2f;
+    //public float smoothSpeed = 10f;
+    public float yOffset = 1f;
+    
+
+    /*[Header("Camera")]
+    public Transform targetObject;
+    public float smoothSpeed = 10f;
+    public float yOffset = 1f;
+    public Vector3 cameraOffset;
+    */
+    
+
     void Update()
     {
-        transform.position = new Vector3(player.position.x, player.position.y, transform.position.z);
+        transform.position = new Vector3(player.position.x, yOffset, transform.position.z);
+      // Vector3 newPosition = new Vector3(player.position.x, player.position.y + yOffset, -10f );
+      // transform.position = Vector3.Lerp(transform.position,newPosition,followSpeed * Time.deltaTime);
         // transform.postion means the position of the camera in world space
         // we are taking the cameras position and we are equaling it to the players position in world space on the x and y axis
     }
+    
+
+    void LateUpdate()
+    {
+
+        /*Vector3 desiredCameraPosition = targetObject.position + yOffset;
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredCameraPosition, smoothSpeed * Time.deltaTime);
+        transform.position = smoothedPosition;
+
+
+        transform.LookAt(targetObject);
+       */ 
+
+      // Vector3 newPosition = new Vector3(player.position.x, player.position.y + yOffset, -20f );
+       //transform.position = Vector3.Slerp(transform.position,newPosition,followSpeed * Time.deltaTime);
+      
+
+    }
+    
 }
---
+++ Semester 1 game/Assets/Scripts/PauseGame.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseGame : MonoBehaviour
+{
+    public static bool gameIsPaused;
+    public GameObject playButton;
+    public GameObject pauseButton;
+     public GameObject pausePanel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playButton.SetActive(false);
+        pauseButton.SetActive(true);
+         pausePanel.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            gameIsPaused = !gameIsPaused;
+            PauseTheGame();
+
+        }
+        */
+    }
+
+   public void GamePaused()
+    {      
+            Time.timeScale = 0f;
+            playButton.SetActive(true);
+            pauseButton.SetActive(false);  
+            pausePanel.SetActive(true);
+       
+    }
+   public void PlayGame()
+    {
+        Time.timeScale = 1f;
+        playButton.SetActive(false);
+        pauseButton.SetActive(true);
+        pausePanel.SetActive(false);
+
+    }
+
+    public void Menu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+
+    /*void PauseTheGame()
+    {
+        if (gameIsPaused)
+        {
+            Time.timeScale = 0f;
+            playButton.SetActive(true);
+            pauseButton.SetActive(false);
+
+        }
+        else
+        {
+            Time.timeScale = 1f;
+            playButton.SetActive(false);
+            pauseButton.SetActive(true);
+        }
+    }
+    */
+}
